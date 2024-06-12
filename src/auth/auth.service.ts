fe147c25f21eec33fd3751daac3a249b7dd9f7a0
abrshewube <abrshefru1427@gmail.com>
2024-06-12 13:57:05 -0700

    done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -163,26 +163,28 @@ generateVerificationCode(): string {
   }
   async updateProfile(token: string, updateProfileDto: UpdateProfileDto): Promise<Profile> {
     try {
-      // Validate the token to extract the user's email
       const decodedToken = await this.validateToken(token);
       const userEmail = decodedToken.email;
   
-      // Find the profile based on the user's email
-      const profile = await this.profileModel.findOne({ user: userEmail });
+      // Find the user based on the email
+      const user = await this.userModel.findOne({ email: userEmail });
+      if (!user) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+  
+      // Find the profile based on the user's _id
+      const profile = await this.profileModel.findOne({ user: user._id });
       if (!profile) {
         throw new HttpException('Profile not found', HttpStatus.NOT_FOUND);
       }
   
       // Update the profile fields
       Object.assign(profile, updateProfileDto);
-  
-      // Save the updated profile
       return profile.save();
     } catch (error) {
-      throw new HttpException(error.message || 'Failed to update profile.', HttpStatus.BAD_REQUEST);
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
-  
   
   // async getProfile(token: string): Promise<Profile | undefined> {
   //   try {
